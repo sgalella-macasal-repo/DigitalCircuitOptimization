0fd2b21c2c4cad931ebb5c6bf4e3805dac67e860
sgalella <santi.galella@gmail.com>
2020-08-29 16:24:39 +0200

    Remove random seeds
    Update parameters and remove seeds for rng.

--- tests/tests2inputs.m
+++ tests/tests2inputs.m
@@ -8,9 +8,6 @@
 % Add path to optimizer folder
 addpath('../src/');
 
-% Set the random seed for controlling rng
-runSeed = 4321;
-
 % Set the parameters
 Params = struct();
 Params.iInfo = 250;
@@ -18,68 +15,58 @@ Params.nGates = 10;
 Params.nMutations = 500;
 Params.nIndividuals = 100;
 Params.nInputs = 2;
-Params.nIterations = 2000;
-Params.mutationRate = 0.1;
+Params.nIterations = 1000;
+Params.mutationRate = 0.3;
 Params.nComponents = Params.nGates + Params.nInputs;
 Params.isDisplay = false;
 Params.isPlot = false;
 
 %% Test1
-rng(runSeed);
 targetOutput = [0 0 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test2
-rng(runSeed);
 targetOutput = [0 0 1 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test3
-rng(runSeed);
 targetOutput = [0 0 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test4
-rng(runSeed);
 targetOutput = [0 1 0 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test5
-rng(runSeed);
 targetOutput = [0 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test6
-rng(runSeed);
 targetOutput = [0 1 1 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test7
-rng(runSeed);
 targetOutput = [0 1 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test8
-rng(runSeed);
 targetOutput = [1 0 1 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test9
-rng(runSeed);
 targetOutput = [1 0 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test10
-rng(runSeed);
 targetOutput = [1 1 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
--- tests/tests3inputs.m
+++ tests/tests3inputs.m
@@ -8,78 +8,65 @@
 % Add path to optimizer folder
 addpath('../src/');
 
-% Set the random seed for controlling rng
-runSeed = 4321;
-
 % Set the parameters
 Params = struct();
 Params.iInfo = 250;
 Params.nGates = 10;
 Params.nMutations = 500;
-Params.nIndividuals = 100;
+Params.nIndividuals = 300;
 Params.nInputs = 3;
-Params.nIterations = 2000;
-Params.mutationRate = 0.1;
+Params.nIterations = 3000;
+Params.mutationRate = 0.3;
 Params.nComponents = Params.nGates + Params.nInputs;
 Params.isDisplay = false;
 Params.isPlot = false;
 
 %% Test1
-rng(runSeed);
 targetOutput = [1 1 1 0 0 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test2
-rng(runSeed);
 targetOutput = [0 1 1 1 1 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test3
-rng(runSeed);
 targetOutput = [0 0 0 1 1 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test4
-rng(runSeed);
 targetOutput = [1 1 1 1 1 1 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test5
-rng(runSeed);
 targetOutput = [0 0 0 0 0 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test6
-rng(runSeed);
 targetOutput = [1 0 0 0 0 0 0 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test7
-rng(runSeed);
 targetOutput = [1 0 1 0 1 0 1 0];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test8
-rng(runSeed);
 targetOutput = [0 1 0 1 0 1 0 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test9
-rng(runSeed);
 targetOutput = [1 1 1 0 0 1 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
 
 %% Test10
-rng(runSeed);
 targetOutput = [1 1 1 1 0 1 1 1];
 bestIndividual = circuitoptimizer(Params, targetOutput);
 assert(isequal(bestIndividual, targetOutput));
